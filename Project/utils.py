81de735eb8b112dbfd1458fa38cd5d18a7a8c666
AlbertRoca29 <albertrocallevadot@gmail.com>
2024-11-12 09:22:41 +0100

    fix some things

--- Project/utils.py
+++ Project/utils.py
@@ -5,7 +5,6 @@ from scipy.optimize import linprog
 
 
 def emd(a, b, M, numItermax=100000, log=False, center_dual=True, check_marginals=True):
-    # Ensure arrays
     a = np.asarray(a, dtype=np.float64)
     b = np.asarray(b, dtype=np.float64)
     M = np.asarray(M, dtype=np.float64)
@@ -27,12 +26,11 @@ def emd(a, b, M, numItermax=100000, log=False, center_dual=True, check_marginals
     # Normalize b to match the total sum of a
     b = b * a.sum() / b.sum()
 
-    # Linear programming approach for Earth Mover's Distance (Wasserstein distance)
+    # Earth Mover's Distance (Wasserstein distance)
     n, m = M.shape
-    c = M.ravel()  # Cost function (flattened M)
+    c = M.ravel() 
     A_eq = np.zeros((n + m, n * m))
 
-    # Constraints
     for i in range(n):
         A_eq[i, i*m:(i+1)*m] = 1  # Row sums (a)
     for j in range(m):
@@ -40,7 +38,7 @@ def emd(a, b, M, numItermax=100000, log=False, center_dual=True, check_marginals
 
     b_eq = np.hstack([a, b])  # Combine a and b
 
-    # Solve the transportation problem using linear programming
+    # Solve the transportation problem using linear programming (no OT)
     res = linprog(c, A_eq=A_eq, b_eq=b_eq, bounds=(0, None), method='highs', options={'maxiter': numItermax})
 
     if log:
@@ -53,6 +51,50 @@ def emd(a, b, M, numItermax=100000, log=False, center_dual=True, check_marginals
 
     return res.x.reshape(n, m)
 
+
+
+def GaussianW2(m0, m1, Sigma0, Sigma1):
+    # Calculate the Wasserstein-2 distance between two Gaussian distributions
+    Sigma00 = spl.sqrtm(Sigma0)
+    Sigma010 = spl.sqrtm(Sigma00 @ Sigma1 @ Sigma00)
+    return np.linalg.norm(m0 - m1)**2 + np.trace(Sigma0 + Sigma1 - 2 * Sigma010)
+
+
+def GW2(pi0, pi1, mu0, mu1, S0, S1):
+    # Compute the Wasserstein-2 distance between two mixtures of Gaussians using EMD
+    K0, K1, d = mu0.shape[0], mu1.shape[0], mu0.shape[1]
+    S0 = S0.reshape(K0, d, d)
+    S1 = S1.reshape(K1, d, d)
+    
+    # Cost matrix for pairwise Wasserstein-2 distances
+    M = np.zeros((K0, K1))
+    for k in range(K0):
+        for l in range(K1):
+            M[k, l] = GaussianW2(mu0[k], mu1[l], S0[k], S1[l])
+    
+    # Compute the optimal transport plan and distance using EMD
+    wstar = emd(pi0, pi1, M)
+    distGW2 = np.sum(wstar * M)
+    return wstar, distGW2
+
+
+def GaussianMap(m0, m1, Sigma0, Sigma1, x):
+    # Transforms the input vector x from the Gaussian distribution (m0, Sigma0)
+    # to the target distribution (m1, Sigma1) by adjusting both the mean and covariance.
+    
+    d = Sigma0.shape[0] 
+    m0 = m0.reshape(1, d)
+    m1 = m1.reshape(1, d)
+    Sigma0 = Sigma0.reshape(d, d)
+    Sigma1 = Sigma1.reshape(d, d)
+    
+    # Compute the transformation matrix to map from (m0, Sigma0) to (m1, Sigma1)
+    Sigma = np.linalg.inv(Sigma0) @ spl.sqrtm(Sigma0 @ Sigma1)
+    
+    # Apply the transformation to the input vector x
+    Tx = m1 + (x - m0) @ Sigma
+    
+    return Tx
 
 
 def average_filter(u,r):
@@ -87,46 +129,3 @@ def guided_filter(u,guide,r,eps):
     
     q = mean_alph * guide + mean_beta
     return q
-
-
-
-def GaussianW2(m0,m1,Sigma0,Sigma1):
-    # compute the quadratic Wasserstein distance between two Gaussians with means m0 and m1 and covariances Sigma0 and Sigma1
-    Sigma00  = spl.sqrtm(Sigma0)
-    Sigma010 = spl.sqrtm(Sigma00@Sigma1@Sigma00)
-    d        = np.linalg.norm(m0-m1)**2+np.trace(Sigma0+Sigma1-2*Sigma010)
-    return d
-
-
-def GW2(pi0,pi1,mu0,mu1,S0,S1):
-    # return the GW2 discrete map and the GW2 distance between two GMM
-    K0 = mu0.shape[0]
-    K1 = mu1.shape[0]
-    d  = mu0.shape[1]
-    S0 = S0.reshape(K0,d,d)
-    S1 = S1.reshape(K1,d,d)
-    M  = np.zeros((K0,K1))
-    # First we compute the distance matrix between all Gaussians pairwise
-    for k in range(K0):
-        for l in range(K1):
-            M[k,l]  = GaussianW2(mu0[k,:],mu1[l,:],S0[k,:,:],S1[l,:,:])
-    # Then we compute the OT distance or OT map thanks to the OT library
-    wstar     = emd(pi0,pi1,M)         # discrete transport plan
-    distGW2   = np.sum(wstar*M)
-    return wstar,distGW2
-
-
-def GaussianMap(m0,m1,Sigma0,Sigma1,x):
-    # Compute the OT map (evaluated at x) between two Gaussians with means m0 and m1 and covariances Sigma0 and Sigma1 
-    # m0 and m1 must be 2D arrays of size 1xd
-    # Sigma0 and Sigma1 must be 2D arrays of size dxd
-    # x can be a matrix of size n x d,
-    # each column of x is a vector to which the function is applied
-    d = Sigma0.shape[0]
-    m0 = m0.reshape(1,d)
-    m1 = m1.reshape(1,d)
-    Sigma0 = Sigma0.reshape(d,d)
-    Sigma1 = Sigma1.reshape(d,d)
-    Sigma  = np.linalg.inv(Sigma0)@spl.sqrtm(Sigma0@Sigma1)
-    Tx        = m1+(x-m0)@Sigma
-    return Tx
